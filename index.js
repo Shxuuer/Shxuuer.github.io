e44eecf7a63301290f24325b7c157a466beae8bd
Shxuuer <a2711492759@163.com>
2024-11-15 00:42:21 +0800

    图片转向cdn

--- index.js
+++ index.js
@@ -1,19 +1,11 @@
+const imgBed = "https://cdn.jsdelivr.net/gh/Shxuuer/picx-images-hosting@master/"
+
 // 禁止右键、禁止复制
 document.oncontextmenu = function () { return false }
 document.onselectstart = function () { return false }
 
-if (window.innerWidth < window.innerHeight) {
-    document.getElementById("cover-img").src="./img/cover-m.jpg"
-}
-
-// 窗口大小改变时
-window.onresize = function () {
-    if (window.innerWidth < window.innerHeight) {
-        document.getElementById("cover-img").src="./img/cover-m.jpg"
-    } else {
-        document.getElementById("cover-img").src="./img/cover.jpg"
-    }
-}
+// 窗口大小改变时更换封面
+document.getElementById("cover-img").src = imgBed + (window.innerWidth < window.innerHeight?"cover-m.jpg":"cover.jpg")
 
 // 滚动到下一页
 document.getElementById("scroll-down-icon").addEventListener('click', function () {
@@ -32,7 +24,7 @@ fetch("./space.json").then(response => response.json()).then(data => {
         space.innerHTML += `
             <div class="space-card">
                 <div>
-                    <img src="./img/space/${element.img}" alt="">
+                    <img src="${imgBed}space/${element.img}" alt="">
                 </div>
                 <div>
                     <p>${element.title}</p>

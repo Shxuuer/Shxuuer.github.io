3bdfa90bf0a951c0519b33f629cdcb2743940de8
Shxuuer <shenxu@bupt.edu.cn>
2024-11-14 22:30:49 +0800

    add space

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 let height = window.innerHeight
-console.log(height);
 let width = window.innerWidth
+
+// 禁止右键、禁止复制
+document.oncontextmenu = function () { return false }
+document.onselectstart = function () { return false }
+
 // 封面自适应
 window.onresize = function () {
     height = window.innerHeight
     width = window.innerWidth
-    const cover = document.getElementById("cover");
-    cover.style.height = height + "px";
-    cover.style.width = width + "px";
+    const cover = document.getElementById("cover")
+    cover.style.height = height + "px"
+    cover.style.width = width + "px"
 }
 
-
-document.addEventListener('scroll', function (e) {
-    let scroll = window.scrollY;
-    const title = document.getElementById("title");
-    title.style.transform = "scale(" + (1 - scroll / height) + ")";
-    title.style.marginTop = (height * 0.3 + height * 0.7 * (scroll / height)) + "px";
+// 滚动到下一页
+document.getElementById("scroll-down-icon").addEventListener('click', function () {
+    window.scrollTo({
+        top: height,
+        behavior: 'smooth'
+    })
 })
+
+// 获取空间信息
+const space = document.getElementById("space-container")
+fetch("./space.json")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        if (!data) { return}
+        data.forEach(element => {
+            // space
+        });
+    })
+

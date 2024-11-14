65d6c87f92085939fd21b89862a70ea6202d9bb8
Shxuuer <a2711492759@163.com>
2024-11-14 23:51:32 +0800

    v1.0

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ window.onresize = function () {
     height = window.innerHeight
     width = window.innerWidth
     const cover = document.getElementById("cover")
-    cover.style.height = height + "px"
-    cover.style.width = width + "px"
+    // cover.style.height = height + "px"
+    // cover.style.width = width + "px"
 }
 
 // 滚动到下一页
@@ -24,13 +24,23 @@ document.getElementById("scroll-down-icon").addEventListener('click', function (
 
 // 获取空间信息
 const space = document.getElementById("space-container")
-fetch("./space.json")
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if (!data) { return}
-        data.forEach(element => {
-            // space
-        });
-    })
+fetch("./space.json").then(response => response.json()).then(data => {
+    if (!data) { return }
+    const space = document.getElementById("space-container")
+    data.news.forEach(element => {
+        space.innerHTML += `
+            <div class="space-card">
+                <div>
+                    <img src="./img/space/${element.img}" alt="">
+                </div>
+                <div>
+                    <p>${element.title}</p>
+                </div>
+                <div>
+                    <span>${element.date}</span>
+                </div>
+            </div>
+        `
+    });
+})
 

7ba0c58f5d2304cbdd299ba897f1ec13471ac7e4
Shxuuer <a2711492759@163.com>
2024-11-14 19:28:13 +0800

    init

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
+let height = window.innerHeight
+console.log(height);
+let width = window.innerWidth
 // 封面自适应
 window.onresize = function () {
-    height = document.body.clientHeight;
-    width = document.body.clientWidth;
+    height = window.innerHeight
+    width = window.innerWidth
     const cover = document.getElementById("cover");
     cover.style.height = height + "px";
     cover.style.width = width + "px";
-
-    const title = document.getElementById("title");
-    title.style.height = height + "px";
-    title.style.backgroundColor = "red";
 }
+
+
+document.addEventListener('scroll', function (e) {
+    let scroll = window.scrollY;
+    const title = document.getElementById("title");
+    title.style.transform = "scale(" + (1 - scroll / height) + ")";
+    title.style.marginTop = (height * 0.3 + height * 0.7 * (scroll / height)) + "px";
+})

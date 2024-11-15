fce04a3a271c4259e98e12e8a27d4fc719700f5e
Shxuuer <shenxu@bupt.edu.cn>
2024-11-15 13:02:42 +0800

    格式化

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const IMG_BED = "https://cdn.jsdelivr.net/gh/Shxuuer/picx-images-hosting@master/"
 
 // 禁止右键、禁止复制、封边图片
+function changeCoverImg() {
+    document.getElementById("cover-img").src = `${IMG_BED}cover${window.innerWidth < window.innerHeight?"-m":""}.jpg`
+}
+changeCoverImg()
 document.oncontextmenu = ()=>false
 document.onselectstart = ()=>false
-document.getElementById("cover-img").src = `${IMG_BED}cover${window.innerWidth < window.innerHeight?"-m":""}.jpg`
+window.onresize = changeCoverImg
 
 // 滚动到下一页
 document.getElementById("scroll-down-icon").addEventListener('click', () => {
@@ -28,17 +32,14 @@ fetch("./space.json").then(response => response.json()).then(data => {
     })
 })
 
-fetch("./githubInfo/1.svg").then(response => response.text()).then(data => {
-    if (!data) { return }
-    document.getElementById("github-container").innerHTML += data
+// 获取Github信息
+const svgs = ['./githubInfo/1.svg', './githubInfo/2.svg']
+svgs.forEach(svg => {
+    fetch(svg).then(response => response.text()).then(data => {
+        if (!data) { return }
+        document.getElementById("github-container").innerHTML += data
+    })
 })
-
-fetch("./githubInfo/2.svg").then(response => response.text()).then(data => {
-    if (!data) { return }
-    document.getElementById("github-container").innerHTML += data
-})
-
-
 
 // 窗口加载完后图片逐渐拉远
 window.onload = function () {
